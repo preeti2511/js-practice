1b2594a9046af588f4d1675741be50c377f76689
preeti2511 <73769931+preeti2511@users.noreply.github.com>
2024-02-07 12:19:00 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js code as a newer version
+//alert(3+3) we are using node js not browser
+
+// readibility
+//console.log(3+3); console.log(4) avoid syntex like this
+//console.log(3
+//         +3) don't do like this we need to take care of readibility and indentation
+
+// documentation by mdn is easy tc.39.s (original documentation) ecma  https://tc39.es/ecma262/#sec-numbers-and-dates
+ 
+let name="prabh"
+let age= 23
+let isLoggedin= true
+
+
+//number range=53 or 52
+//bigint for big number
+//string =""
+//boolean= true or false
+//null= standalone value
+//undefined
+//symbol=unique
+//object
+console.log(typeof null)// object
+
+
