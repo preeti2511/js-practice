1071180ef036f4d9718d690f1a9b77faaaf34b70
preeti2511 <73769931+preeti2511@users.noreply.github.com>
2024-02-07 12:44:47 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,26 @@
+let score= "33ab"
+console.log(typeof(score))
+let valueInnumber = Number(score)
+console.log(typeof(valueInnumber))
+console.log(valueInnumber)//In JavaScript, NaN is short for "Not-a-Number". In JavaScript, NaN is a number that is not a legal number. The Number. isNaN() method returns true if the value is NaN , and the type is a Number.
+
+//number conversion =  Number
+//33=>33
+//"33abc"=>NaN
+//true= 1
+//false=0
+
+let isLogg = 1
+let convert = Boolean(isLogg)
+console.log(convert)
+// for boolean conversion we are using boolean
+//1=true
+//0=false
+//empty=false
+//value add =true e.g "prabh"
+
+let sum = 33
+let stringNumber = String(sum)
+console.log(typeof(stringNumber))
+console.log(stringNumber)
+// converting into string we use String
